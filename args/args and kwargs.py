c2c3ab99ca91ef14a98c5661e583c1ad033c72a1
Ramis <Sir.ramis@mail.ru>
2023-01-29 14:51:04 +0300

    Создал args and kwargs.py

---
+++ args/args and kwargs.py
@@ -0,0 +1,15 @@
+def is_cat_here(text):
+    if 'cat' in text.lower():
+        return True
+    else:
+        return False
+print(is_cat_here('is Cat here'))
+
+
+def is_cat_here(text):
+    if 'cat' in text.lower():
+        return True
+    else:
+        return False
+print(is_cat_here('is Cat here'))
+
