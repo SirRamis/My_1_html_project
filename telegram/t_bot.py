bfe4ea6d8ea49860d7cdb1708a480a80aa4f56cb
Ramis <Sir.ramis@mail.ru>
2023-02-08 18:50:01 +0300

    Добавляю в телеграмм бот задачки

--- telegram/t_bot.py
+++ telegram/t_bot.py
@@ -1,8 +1,11 @@
 import telebot
+import random
 
 token = "6280256013:AAFxiErfho7BYdIwMp7gXva1xgacj4vlW60"
 
 bot = telebot.TeleBot(token)
+
+RANDOM_TASKS = ["Почитать Коран", "Сделать зикр", "Позвонить родственникам", "Сделать дога"]
 
 HELP = """
 /help - напечатать справку по программе.
@@ -12,7 +15,6 @@ HELP = """
 
 tasks = {}
 
-
 def add_todo(date, task):
     if date in tasks:
         tasks[date].append(task)
@@ -20,15 +22,38 @@ def add_todo(date, task):
         tasks[date] = []
         tasks[date].append(task)
 
-
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.send_message(message.chat.id, HELP)
 
+@bot.message_handler(commands=["add", "todo"])
+def add(message):
+    command = message.text.split(maxsplit=2)
+    date = command[1].lower()
+    task = command[2]
+    add_todo(date,task)
+    text = "Задача " + task + " добавлена на дату " + date
+    bot.send_message(message.chat.id, text)
 
-@bot.message_handler(content_types=["text"])
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=["random"])
+def random_add(message):
+    date = "сегодня"
+    task = random.choice(RANDOM_TASKS)
+    add_todo(date,task)
+    text = "Задача " + task + " добавлена на дату " + date
+    bot.send_message(message.chat.id, text)
 
+@bot.message_handler(commands=["show", "print"])
+def show(message)
+    command = message.text.split(maxsplit=1)
+    date = command[1].lower()
+    text = ""
+    if date in tasks:
+        text = date.upper() + "\n"
+        for task in tasks[date]:
+            text = text + "-" + task + "\n"
+    else:
+        text = "Задача на эту дату нет"
+    bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
